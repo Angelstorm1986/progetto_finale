2fc02b34a824cedcd0ddb7b7bbed56e14c031ae3
Angelstorm1986 <matteo.nichelini@gmail.com>
2022-07-12 15:05:57 +0200

    added surname

--- public/js/app.js
+++ public/js/app.js
@@ -49562,8 +49562,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\esercizi\laravel\final-project\progetto_finale\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\esercizi\laravel\final-project\progetto_finale\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\MAMP\htdocs\esercizi_php\classe59\progetto_finale\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\MAMP\htdocs\esercizi_php\classe59\progetto_finale\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
