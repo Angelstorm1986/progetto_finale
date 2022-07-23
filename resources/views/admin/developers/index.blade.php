24e017b006a738bd012f8c3e72ccb139dfb50471
Angelstorm1986 <matteo.nichelini@gmail.com>
2022-07-23 18:16:48 +0200

    modify index

--- resources/views/admin/developers/index.blade.php
+++ resources/views/admin/developers/index.blade.php
@@ -42,41 +42,43 @@
 <div class="row">
     @foreach ($users as $user)
         @foreach($developers as $developer)
-            <div class="card-container mx-3">
-                <span class="pro">PRO</span>
-                <a class="text-decoration-none text-reset" href="{{route('admin.developers.show', $developer->id)}}">
-                    <img class="round" src="{{ asset('storage/uploads/' . $developer->photo) }} ? {{ asset('storage/uploads/' . $developer->photo) }} : {{ asset('storage/uploads/project-user.png') }}
-                    " alt="{{ $user->name }} {{ $user->surname }}">
-                </a>
-                <a class="text-decoration-none text-reset" href="{{route('admin.developers.show', $developer->id)}}">
-                    <h3>{{ $user->name }} {{ $user->surname }}</h3>
-                </a>        
-                <h6>{{ $user->address }}</h6>
-                <span class="phone"><small>Tel.number: {{ $developer->phone_number }}</small></span>
+            @if($developer->user_id == $user->id)
+                <div class="card-container mx-3">
+                    <span class="pro">PRO</span>
+                    <a class="text-decoration-none text-reset" href="{{route('admin.developers.show', $developer->id)}}">
+                        <img class="round" src="{{ asset('storage/uploads/' . $developer->photo) }} ? {{ asset('storage/uploads/' . $developer->photo) }} : {{ asset('storage/uploads/project-user.png') }}
+                        " alt="{{ $user->name }} {{ $user->surname }}">
+                    </a>
+                    <a class="text-decoration-none text-reset" href="{{route('admin.developers.show', $developer->id)}}">
+                        <h3>{{ $user->name }} {{ $user->surname }}</h3>
+                    </a>        
+                    <h6>{{ $user->address }}</h6>
+                    <span class="phone"><small>Tel.number: {{ $developer->phone_number }}</small></span>
 
-                <div class="buttons">
-                    <button class="primary">
-                        Message
-                    </button>
-                    <button class="primary ghost">
-                        Reviews
-                    </button>
+                    <div class="buttons">
+                        <button class="primary">
+                            Message
+                        </button>
+                        <button class="primary ghost">
+                            Reviews
+                        </button>
+                    </div>
+                    <div class="skills">
+                        <h6>Description:</h6>
+                        <p>{{ $developer->description }}</p>
+                        <h6>Skills:</h6>
+                        <p><strong>{{ $developer->skills }}</strong></p>
+                        <div class="language">
+                            <h6>Languages:</h6>
+                            <ul>
+                                @foreach ($developer->languages as $language)
+                                    <li>{{$language->name}}</li>
+                                @endforeach
+                            </ul>
+                        </div>    
+                    </div>
                 </div>
-                <div class="skills">
-                    <h6>Description:</h6>
-                    <p>{{ $developer->description }}</p>
-                    <h6>Skills:</h6>
-                    <p><strong>{{ $developer->skills }}</strong></p>
-                    <div class="language">
-                        <h6>Languages:</h6>
-                        <ul>
-                            @foreach ($developer->languages as $language)
-                                <li>{{$language->name}}</li>
-                            @endforeach
-                        </ul>
-                    </div>    
-                </div>
-            </div>
+            @endif
         @endforeach
     @endforeach
 </div>
--- resources/views/guest/developers/index.blade.php
+++ resources/views/guest/developers/index.blade.php
@@ -18,42 +18,44 @@
     <div class="row">
         @foreach ($users as $user)
             @foreach($developers as $developer)
-                <div class="card-container mx-3">
-                    <span class="pro">PRO</span>
-                    <div class="img-box">
+                @if($developer->user_id == $user->id)
+                    <div class="card-container mx-3">
+                        <span class="pro">PRO</span>
+                        <div class="img-box">
+                            <a class="text-decoration-none text-reset" href="{{route('guest.developers.show', $developer->id)}}">
+                                <img class="round" src=" {{ asset('storage/' . $developer->photo) }} " alt="{{ $user->name }} {{ $user->surname }}">
+                            </a>
+                        </div>
                         <a class="text-decoration-none text-reset" href="{{route('guest.developers.show', $developer->id)}}">
-                            <img class="round" src=" {{ asset('storage/' . $developer->photo) }} " alt="{{ $user->name }} {{ $user->surname }}">
-                        </a>
+                            <h3>{{ $user->name }} {{ $user->surname }}</h3>
+                        </a>        
+                        <h6>{{ $user->address }}</h6>
+                        <span class="phone"><small>Tel.number: {{ $developer->phone_number }}</small></span>
+        
+                        <div class="buttons">
+                            <button class="primary">
+                                Message
+                            </button>
+                            <button class="primary ghost">
+                                Reviews
+                            </button>
+                        </div>
+                        <div class="skills">
+                            <h6>Description:</h6>
+                            <p>{{ $developer->description }}</p>
+                            <h6>Skills:</h6>
+                            <p><strong>{{ $developer->skills }}</strong></p>
+                            <div class="language">
+                                <h6>Languages:</h6>
+                                <ul>
+                                    @foreach ($developer->languages as $language)
+                                        <li>{{$language->name}}</li>
+                                    @endforeach
+                                </ul>
+                            </div>    
+                        </div>
                     </div>
-                    <a class="text-decoration-none text-reset" href="{{route('guest.developers.show', $developer->id)}}">
-                        <h3>{{ $user->name }} {{ $user->surname }}</h3>
-                    </a>        
-                    <h6>{{ $user->address }}</h6>
-                    <span class="phone"><small>Tel.number: {{ $developer->phone_number }}</small></span>
-    
-                    <div class="buttons">
-                        <button class="primary">
-                            Message
-                        </button>
-                        <button class="primary ghost">
-                            Reviews
-                        </button>
-                    </div>
-                    <div class="skills">
-                        <h6>Description:</h6>
-                        <p>{{ $developer->description }}</p>
-                        <h6>Skills:</h6>
-                        <p><strong>{{ $developer->skills }}</strong></p>
-                        <div class="language">
-                            <h6>Languages:</h6>
-                            <ul>
-                                @foreach ($developer->languages as $language)
-                                    <li>{{$language->name}}</li>
-                                @endforeach
-                            </ul>
-                        </div>    
-                    </div>
-                </div>
+                @endif
             @endforeach
         @endforeach
     </div>
